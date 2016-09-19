89db2895e2f8e67ec2fe6a4b5004b338357d21a0
www-data <sunnysideupdevs@gmail.com>
2016-09-19 18:49:38 +1200

    fix ups

--- _config.php
+++ _config.php
@@ -9,4 +9,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START ecommerce_club_order MODULE ----------------===================
 //===================---------------- END ecommerce_club_order MODULE ----------------===================
-
--- code/model/AddUpProductsToOrderPageStatusLog.php
+++ code/model/AddUpProductsToOrderPageStatusLog.php
@@ -6,35 +6,42 @@
  * It is created by the order and it signifies to the OrderStep to continue to the next step.
  **/
 
-class AddUpProductsToOrderPageStatusLog extends OrderStatusLog {
+class AddUpProductsToOrderPageStatusLog extends OrderStatusLog
+{
+    public static $defaults = array(
+        'EmailCustomer' => false,
+        'EmailSent' => false,
+        'InternalUseOnly' => false
+    );
 
-	public static $defaults = array(
-		'EmailCustomer' => false,
-		'EmailSent' => false,
-		'InternalUseOnly' => false
-	);
+    /**
+    *
+    *@return Boolean
+    **/
+    public function canCreate($member = null)
+    {
+        return true;
+    }
 
-	/**
-	*
-	*@return Boolean
-	**/
-	public function canCreate($member = null) {
-		return true;
-	}
+    public static $singular_name = "Club Order Detail";
+    public function i18n_singular_name()
+    {
+        return _t("AddUpProductsToOrderPageStatusLog.ClubOrderDetail", "Club Order Detail");
+    }
 
-	public static $singular_name = "Club Order Detail";
-		function i18n_singular_name() { return _t("AddUpProductsToOrderPageStatusLog.ClubOrderDetail", "Club Order Detail");}
+    public static $plural_name = "Club Orders Details";
+    public function i18n_plural_name()
+    {
+        return _t("AddUpProductsToOrderPageStatusLog.ClubOrdersDetails", "Club Orders Details");
+    }
 
-	public static $plural_name = "Club Orders Details";
-		function i18n_plural_name() { return _t("AddUpProductsToOrderPageStatusLog.ClubOrdersDetails", "Club Orders Details");}
-
-	/**
-	*
-	*@return FieldSet
-	**/
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		return $fields;
-	}
-
+    /**
+    *
+    *@return FieldSet
+    **/
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        return $fields;
+    }
 }
